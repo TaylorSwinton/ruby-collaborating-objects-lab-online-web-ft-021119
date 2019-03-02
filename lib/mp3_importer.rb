c0515092d37700203af808595b5ac44e7f8e7554
Taylor Swinton <taylorswinton1@gmail.com>
2019-03-02 00:30:20 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,7 +9,7 @@ class MP3Importer
   def files
     #@found_files = Dir.entries(@path) #noramlizes the filename to just the mp3 filename with no path
     #@found_files = Dir.glob("#{@path}/*.mp3") #loads all the mp3 files in the path directory
-    @files ||= Dir.entries(@path).select {|filename| File.directory?(filename) && filename.end_with?(".mp3")}
+    @files ||= Dir.entries(@path).select {|filename| !File.directory?(filename) && filename.end_with?(".mp3")}
   end
 
   def import
